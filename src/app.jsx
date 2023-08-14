d5a9cc232439cec775d39421093829895fd28a88
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2023-08-14 17:48:14 +0000

    class converted

--- src/app.jsx
+++ src/app.jsx
@@ -4,10 +4,46 @@ import './app.scss';
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
+
 import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
+import { useState } from 'react';
+
+// function App() {
+//   const [data, setData] = useState(null);
+//   const [requestParams, setRequestParams] = useState({});
+
+//   const callApi = (requestParams) => {
+//     // Mock output
+//     const newData = {
+//       count: 2,
+//       results: [
+//         { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+//         { name: 'fake thing 2', url: 'http://fakethings.com/2' },
+//       ],
+//     };
+//     setData(newData);
+//     setRequestParams(requestParams);
+//   }
+
+//   return (
+//     <React.Fragment>
+//       <Header />
+//       <div>Request Method: {requestParams.method}</div>
+//       <div>URL: {requestParams.url}</div>
+//       <Form handleApiCall={callApi} />
+//       <Results data={data} />
+//       <Footer />
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
+
+
 
 class App extends React.Component {
 
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,22 +1,17 @@
-import React from 'react';
-
 import './form.scss';
 
-class Form extends React.Component {
-
-  handleSubmit = e => {
+function Form(props){
+    let handleSubmit = e => {
     e.preventDefault();
     const formData = {
       method:'GET',
       url: 'https://pokeapi.co/api/v2/pokemon',
     };
-    this.props.handleApiCall(formData);
+    props.handleApiCall(formData);
   }
-
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
+  return(
+    <>
+        <form onSubmit={handleSubmit}>
           <label >
             <span>URL: </span>
             <input name='url' type='text' />
@@ -29,9 +24,48 @@ class Form extends React.Component {
             <span id="delete">DELETE</span>
           </label>
         </form>
-      </>
-    );
-  }
+    </>
+  )
 }
 
-export default Form;
+export default Form
+
+
+
+// import React from 'react';
+
+// import './form.scss';
+
+// class Form extends React.Component {
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const formData = {
+//       method:'GET',
+//       url: 'https://pokeapi.co/api/v2/pokemon',
+//     };
+//     this.props.handleApiCall(formData);
+//   }
+
+//   render() {
+//     return (
+//       <>
+        // <form onSubmit={this.handleSubmit}>
+        //   <label >
+        //     <span>URL: </span>
+        //     <input name='url' type='text' />
+        //     <button type="submit">GO!</button>
+        //   </label>
+        //   <label className="methods">
+        //     <span id="get">GET</span>
+        //     <span id="post">POST</span>
+        //     <span id="put">PUT</span>
+        //     <span id="delete">DELETE</span>
+        //   </label>
+        // </form>
+//       </>
+//     );
+//   }
+// }
+
+// export default Form;
